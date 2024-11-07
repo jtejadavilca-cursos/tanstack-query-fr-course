6c26a3e94629dea7a9c5ca292aa7c61945756ef5
Jose Tejada <jtejadavilca@gmail.com>
2024-11-07 01:11:55 +0000

    Refactoring code

--- src/issues/components/IssueList.tsx
+++ src/issues/components/IssueList.tsx
@@ -1,10 +1,12 @@
 import { FC } from "react";
 import { IssueItem } from "./";
 import { GithubIssue, State } from "../interfaces";
+import { useIssues } from "../../hooks";
+import { LoadingSpiner } from "../../shared/components";
 
 interface IssueListProps {
-    issues: GithubIssue[];
     selectedState: State;
+    selectedLabels: string[];
     onStateChange: (state: State) => void;
 }
 
@@ -12,25 +14,40 @@ const getActiveState = (selectedState: State, state: State) => {
     return selectedState === state ? "active" : "";
 };
 
-export const IssueList: FC<IssueListProps> = ({ issues, selectedState, onStateChange }) => {
+export const IssueList: FC<IssueListProps> = ({ selectedState, selectedLabels, onStateChange }) => {
+    const { issuesQuery, currentPage, nextPage, previousPage, prefetchData, resetPage } = useIssues({
+        state: selectedState,
+        selectedLabels,
+    });
+    const issues: GithubIssue[] = issuesQuery.data ?? [];
+
+    if (issuesQuery.isLoading) {
+        return <LoadingSpiner />;
+    }
+
+    const handleChageState = (state: State) => {
+        resetPage();
+        onStateChange(state);
+    };
+
     return (
         <>
             {/* Botones de All, Open, Closed */}
             <div className="flex gap-4">
                 <button
-                    onClick={() => onStateChange(State.All)}
+                    onClick={() => handleChageState(State.All)}
                     className={"btn " + getActiveState(selectedState, State.All)}
                 >
                     All
                 </button>
                 <button
-                    onClick={() => onStateChange(State.Open)}
+                    onClick={() => handleChageState(State.Open)}
                     className={"btn " + getActiveState(selectedState, State.Open)}
                 >
                     Open
                 </button>
                 <button
-                    onClick={() => onStateChange(State.Close)}
+                    onClick={() => handleChageState(State.Close)}
                     className={"btn " + getActiveState(selectedState, State.Close)}
                 >
                     Closed
@@ -42,6 +59,25 @@ export const IssueList: FC<IssueListProps> = ({ issues, selectedState, onStateCh
                 {issues.map((issue) => (
                     <IssueItem key={issue.id} issue={issue} />
                 ))}
+            </div>
+
+            <div className="flex justify-between items-center">
+                <button
+                    className="bg-blue-500 rounded-md hover:bg-blue-700 transition-all text-white font-bold py-2 px-4"
+                    disabled={currentPage === 1}
+                    onClick={previousPage}
+                    onMouseMove={() => prefetchData(-1)}
+                >
+                    Anteriores
+                </button>
+                <span>{currentPage}</span>
+                <button
+                    className="bg-blue-500 rounded-md hover:bg-blue-700 transition-all text-white font-bold py-2 px-4"
+                    onClick={nextPage}
+                    onMouseMove={() => prefetchData(1)}
+                >
+                    Siguientes
+                </button>
             </div>
         </>
     );
--- src/issues/views/ListView.tsx
+++ src/issues/views/ListView.tsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import { useIssues } from "../../hooks";
-import { LoadingSpiner } from "../../shared/components";
 import { IssueList, LabelPicker } from "../components";
 import { State } from "../interfaces";
 
@@ -8,55 +6,22 @@ export const ListView = () => {
     const [state, setState] = useState<State>(State.All);
     const [selectedLabels, setSelectedLabels] = useState<string[]>([]);
 
-    const { issuesQuery, currentPage, nextPage, previousPage, prefetchData, resetPage } = useIssues({
-        state,
-        selectedLabels,
-    });
-
-    const issues = issuesQuery.data ?? [];
-
     const onLabelSelected = (label: string) => {
         if (selectedLabels.includes(label)) {
             setSelectedLabels(selectedLabels.filter((l) => l !== label));
         } else {
             setSelectedLabels([...selectedLabels, label]);
         }
-        resetPage();
     };
 
     const handleStageChange = (state: State) => {
         setState(state);
-        resetPage();
     };
 
     return (
         <div className="grid grid-cols-1 sm:grid-cols-3 mt-5">
             <div className="col-span-1 sm:col-span-2">
-                {issuesQuery.isLoading ? (
-                    <LoadingSpiner />
-                ) : (
-                    <>
-                        <IssueList issues={issues} onStateChange={handleStageChange} selectedState={state} />
-                        <div className="flex justify-between items-center">
-                            <button
-                                className="bg-blue-500 rounded-md hover:bg-blue-700 transition-all text-white font-bold py-2 px-4"
-                                disabled={currentPage === 1}
-                                onClick={previousPage}
-                                onMouseMove={() => prefetchData(-1)}
-                            >
-                                Anteriores
-                            </button>
-                            <span>{currentPage}</span>
-                            <button
-                                className="bg-blue-500 rounded-md hover:bg-blue-700 transition-all text-white font-bold py-2 px-4"
-                                onClick={nextPage}
-                                onMouseMove={() => prefetchData(1)}
-                            >
-                                Siguientes
-                            </button>
-                        </div>
-                    </>
-                )}
+                <IssueList selectedLabels={selectedLabels} onStateChange={handleStageChange} selectedState={state} />
             </div>
 
             <div className="col-span-1 px-2">
